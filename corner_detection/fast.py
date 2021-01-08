dc7b02e058e72e455960199cea41cabf7a575e25
Jacek <J.Dobrowolski@stud.elka.pw.edu.pl>
2021-01-08 12:35:26 +0100

    adding generating arc

--- corner_detection/fast.py
+++ corner_detection/fast.py
@@ -7,31 +7,71 @@ Corner detection using FAST method.
 import numpy as np
 
 
-def FAST(image: np.ndarray, threshold: int, n: int):
+def _generate_arc(radius: int):
+    arc = set()
+    arc.add((radius, 0))
+    if radius - 1 > 0:
+        for x in range(1, radius):
+            min_value = 1.0
+            for y in range(1, radius + 1):
+                value = abs(((x**2 + y**2) / radius**2) - 1)
+                # print(x, y, value, min_value)
+                if min_value > value:
+                    # print("found min")
+                    min_value = value
+                    min_x, min_y = x, y
+            arc.add((min_x, min_y))
+
+    temp_arc = arc.copy()
+    for x, y in temp_arc:
+        arc.add((y, x))
+
+    temp_arc = arc.copy()
+    for x, y in temp_arc:
+        arc.add((-x, y))
+
+    temp_arc = arc.copy()
+    for x, y in temp_arc:
+        arc.add((x, -y))
+
+    temp_arc = arc.copy()
+    for x, y in temp_arc:
+        arc.add((-x, -y))
+
+    a = np.zeros((radius*2+1, radius*2+1))
+    for x, y in arc:
+        a[radius+x, radius+y] = 1
+
+    print(a)
+
+    return arc
+
+
+def FAST(image: np.ndarray, radius: int, threshold: int, n: float):
     """Finds good corners in grayscale image, using FAST method.
 
     Args:
         image (np.ndarray): Grayscale image in form of 2 dimensional np.ndarray
+        radius (int): radius of a circle around candidate corner.
+                    Must be greater or equals 1.
         threshold (int): Threshold value.
-        n (int): Number of pixels on circle around candidate corner with value
-                    difference greater than threshold.
+        n (int): Fraction of pixels on circle around candidate corner with
+                    value difference greater than threshold.
+                    Must be between 0 and 1.
 
     Returns:
         list: List of tuples containing indexes of good corners
     """
     assert isinstance(image, np.ndarray) and image.ndim == 2, \
         'image must be grayscale image, np.ndarray'
-    # assert isinstance(radius, int), 'radius must be integer'
+    assert isinstance(radius, int), 'radius must be integer'
+    assert radius >= 1, 'radius must be greater or equal 1'
+    assert n <= 1, 'n must be lesser or equal 1'
+    assert radius > 0, 'n must be greater than 0'
 
     corners = list()
-
-    # arc list
-    arcList = [(-1, 3),  (0, 3), (1, 3),
-               (-1, -3), (0, -3), (1, -3),
-               (3, -1),  (3, 0),  (3, 1),
-               (-3, -1), (-3, 0), (-3, 1),
-               (2, 2),   (2, -2), (-2, 2),  (-2, -2)]
-    radius = 3
+    arc = _generate_arc(radius)
+    n = int(n*len(arc))
 
     for x in range(radius, image.shape[0] - radius):
         for y in range(radius, image.shape[1] - radius):
@@ -39,7 +79,7 @@ def FAST(image: np.ndarray, threshold: int, n: int):
             darker_count = 0
             brighter_count = 0
 
-            for arcX, arcY in arcList:
+            for arcX, arcY in arc:
                 diff = int(image[x, y]) - int(image[x + arcX, y + arcY])
                 if diff > threshold:
                     darker_count += 1
--- tests/test_FAST.py
+++ tests/test_FAST.py
@@ -4,27 +4,31 @@ import cv2
 import numpy as np
 
 
-def test_CornerDetection_argument_type_assertions():
-    lenna = cv2.imread("lenna.png")
-
-    with pytest.raises(AssertionError):
-        FAST(lenna, 80, 10)
-
-    with pytest.raises(AssertionError):
-        FAST(1, 80, 10)
-
-    with pytest.raises(AssertionError):
-        FAST([[1, 2], [1, 2]], 80, 10)
-
-    with pytest.raises(AssertionError):
-        FAST(np.zeros(10), 80, 10)
-
-
-def test_CornerDetection_return_value():
+def test_corner_detection_fast_argument_type():
     lenna = cv2.imread("lenna.png")
     lenna_gray = cv2.cvtColor(lenna, cv2.COLOR_BGR2GRAY)
 
-    corners = FAST(lenna_gray, 80, 10)
+    with pytest.raises(AssertionError):
+        FAST(lenna, 3, 80, 0.8)
+
+    with pytest.raises(AssertionError):
+        FAST(1, 3, 80, 0.8)
+
+    with pytest.raises(AssertionError):
+        FAST([[1, 2], [1, 2]], 3, 80, 0.8)
+
+    with pytest.raises(AssertionError):
+        FAST(np.zeros(10), 3, 80, 0.8)
+
+    with pytest.raises(AssertionError):
+        FAST(lenna_gray, 0, 80, 0.8)
+
+
+def test_corner_detection_fast_return_value():
+    lenna = cv2.imread("lenna.png")
+    lenna_gray = cv2.cvtColor(lenna, cv2.COLOR_BGR2GRAY)
+
+    corners = FAST(lenna_gray, 3, 80, 0.8)
 
     assert isinstance(corners, list)
     assert isinstance(corners[0], tuple)
