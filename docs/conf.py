32907ac155f9d2f84087795853540b700e68c322
Jacek <J.Dobrowolski@stud.elka.pw.edu.pl>
2021-01-22 16:08:17 +0100

    adding sphinx extensions

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,9 @@ extensions = [
     'sphinx.ext.napoleon',
     'sphinx.ext.autodoc',
     'sphinx.ext.githubpages',
-    'sphinx.ext.coverage'
+    'sphinx.ext.coverage',
+    'sphinxcontrib.bibtex',
+    'jupyter_sphinx'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -57,3 +59,5 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+bibtex_bibfiles = ['']
